f85b78fa38edd8c900f44bc815e13dbf5d9dc91f
Sam <samuelmk@umich.edu>
2019-07-27 23:32:56 -0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Samuel Kim on 7/27/19.
+//  Copyright © 2019 Samuel Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
